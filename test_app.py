cf3efcaeef50d671f1a0ead0b556379e3b07b5c1
mathguy-r <rohanmukherjee10@gmail.com>
2022-05-20 22:33:27 +0530

    changed test_app.py

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ import time
 if __name__ == "__main__":
     st.title("This is a test Return Calculator")
     p = st.number_input("I want to invest (in INR)", min_value=1000, step=500)
-    f = st.radio("I want to do it",("One time","Monthly"))
+    f = st.radio("I want to do it",("One time","Monthly SIP"))
     r = st.number_input("Yearly Interest Rate", min_value=0.5, step=0.1)
     t = st.slider("I want to invest for (in years)", min_value=1,max_value=10)
     if st.button("Calculate"):
